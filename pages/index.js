d2e227922a0678919bf3ecaaced3293299ca2285
Werner gehrke Junior <werner.junior@hbsis.com.br>
2021-02-18 17:21:06 -0300

    teste 2

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,7 @@
 import {useState} from 'react';
 
 function Home(){
-  return (
-    <>
-    <div>Hello World</div>
-    <Contador />
-    </>
-  )
-}
-
-function Contador(){
-  const [contador, setContador] = useState(0);
-
-  function adicionarContador(){
-    setContador(contador + 1);
-  }
-
-  return (
-    <div>
-      <div>{contador}</div>
-      <button onClick={() => adicionarContador()}>Add</button>
-    </div>
-  )
+  return     <div>Hello World</div>  
 }
 
 export default Home;
