5fba17f712130767a21eff552d39f4010e2d9684
Werner gehrke Junior <werner.junior@hbsis.com.br>
2021-02-18 19:12:02 -0300

    teste 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 // import { Container } from './styles';
 
 const pages: React.FC = () => {
-  return <h1>Teste TS</h1>;
+  return <h1>Teste TSX</h1>;
 }
 
 export default pages;
