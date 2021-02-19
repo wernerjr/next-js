f8fc7be36c4e9521e135b3fd2c881b715e254fb9
Werner gehrke Junior <werner.junior@hbsis.com.br>
2021-02-18 21:20:48 -0300

    teste 6

--- pages/api/time/index.ts
+++ pages/api/time/index.ts
@@ -1,9 +1,13 @@
-const time = (request, response): void => {
+const time = async (request, response): Promise<void> => {
+  const apiSecret = process.env.CONVERTKIT_API_SECRET;
   const dynamicDate = new Date();
 
+  const subscribersResponse = await fetch(`https://api.convertkit.com/v3/subscribers?api_secret=${apiSecret}`)
+  const subscribersResponseJson = await subscribersResponse.json();
   response.json(
     {
-      date: dynamicDate.toUTCString()
+      date: dynamicDate.toUTCString(),
+      subscribers: subscribersResponseJson.total_subscribers
     }
   )
 }
